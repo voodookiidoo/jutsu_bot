3c12eb6b76db9a94a0c2bcf603a17bddfeac8360
voodookiidoo <voodookiidoo@yandex.ru>
2022-05-15 02:31:17 +0300

    libs imported, token file added, token read

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import json
+import requests
+import telebot
 
 
+def main():
+	with open('src/data.txt', 'r') as f:
+		TKN = f.read()
+	print(TKN)
 
-print(20)
-print(20)
+if __name__ == '__main__':
+	main()
