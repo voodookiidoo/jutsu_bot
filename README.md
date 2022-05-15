fe470df00f34ef3a34ec23707ee7a11711499208
voodookiidoo <voodookiidoo@yandex.ru>
2022-05-15 03:15:10 +0300

    added parser.py, added inital keyboard

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+<p>Among us</p>
+
 
--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import json
 import requests
-import telebot
+from telebot import TeleBot
+from telebot.types import Message, ReplyKeyboardMarkup, KeyboardButton
+
+with open('src/data.txt', 'r') as f:
+	TKN = f.read()
+bot = TeleBot(TKN)
 
 
-def main():
-	with open('src/data.txt', 'r') as f:
-		TKN = f.read()
-	print(TKN)
-	print('amogus')
+@bot.message_handler(commands=['start'])
+def start(msg:Message):
+	brd = ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+	his = KeyboardButton(text='Jutsu history')
+	fav = KeyboardButton(text='Favorite jutsu')
+	new = KeyboardButton(text='Learn new jutsu')
+	brd.add(his, fav, new)
+	bot.send_message(msg.chat.id, 'Hello there, fellow ninja.\nWhat are you up to now?', reply_markup=brd)
 
-if __name__ == '__main__':
-	main()
+
+bot.polling()
